25bb3afee9795cca7cb6520d4ecaf3843a6e2f99
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2022-07-04 18:37:33 +0800

    Run Rails 6.0's update task

---
+++ config/initializers/sqlite3.rb
@@ -0,0 +1,4 @@
+# DEPRECATION WARNING: `.represent_boolean_as_integer=` is now always true,
+# so setting this is deprecated and will be removed in Rails 6.1.
+
+# Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
