219bcdac60c064ef7abb09145a4d4085c1bd2dee
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2022-07-09 18:53:03 +0800

    Move jquery (partical) to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
