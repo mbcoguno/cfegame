9fe3434ae846b39e5d71459cfd5a0f67914d886e
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2022-07-10 18:18:48 +0800

    Use CDN jQuery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "@hotwired/turbo-rails";
-import "jquery";
-import "jquery_ujs";
-import "bootstrap";
-import { run, hidden_alert } from "games";
+import "@hotwired/turbo-rails"
+import "jquery"
+import "jquery-ujs"
+import "bootstrap"
+import { run, hidden_alert } from "games"
 
 var app
 
 $(document).on("turbo:load", () => {
   run(app)
 })
+
 $(document).bind("page:load ready", () => {
   hidden_alert()
 })
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "jquery", to: "jquery.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
+pin "jquery-ujs", to: "https://ga.jspm.io/npm:jquery-ujs@1.2.3/src/rails.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin_all_from "app/javascript/games", under: "games"
